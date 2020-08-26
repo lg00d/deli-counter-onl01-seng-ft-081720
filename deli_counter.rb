d8eb500ee3304199df025f97bf2294726a2276e3
elizabeth goodman <egoodman1180@icloud.com>
2020-08-26 12:40:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+
+def line(deli)
+  if deli.empty?
+    binding.pry
+    puts "The line is currently empty."
+  else 
+    binding.pry
+    line_string = "The line is currently:"
+    binding.pry
+    deli.each.with_index(1) do | person, i |
+      binding.pry
+      line_string << " #{i}.   #{person}"
+    end
+    binding.pry
+    puts line_string
+    binding.pry
+  end
+end
